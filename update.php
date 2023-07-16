6d3def7a7f95eb590704c13cf4973289c2af3128
Yasuko kawashima <yasuko.kawashima@gmail.com>
2023-07-16 16:20:36 +0900

    編集

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php 
 $dsn = 'mysql:dbname=heroku_2338003a1917fd8;host=us-cdbr-east-06.cleardb.net;charset=utf8mb4';
-$user = 'bfd69e2febafbf:83fc82e6';
+$user = 'bfd69e2febafbf';
 $password = '83fc82e6';
 
  // submitパラメータの値が存在するとき（「更新」ボタンを押したとき）の処理
